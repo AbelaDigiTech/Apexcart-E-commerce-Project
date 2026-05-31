fe204cc56613077ccbef6b1e4c5ba5b46f28e1f3
kidahosa <idahosakevinaibalegbagbon@gmail.com>
2026-05-31 19:08:38 +0100

    cart routes remove from cart fixed

--- routes/cartRoutes.js
+++ routes/cartRoutes.js
@@ -6,7 +6,7 @@ const protect = require('../middleware/authMiddleware');
 router.use(protect); // All shopping cart management requires logging in first
 
 router.get('/', cartController.getCart);
-router.post('/', cartController.addToCartOrUpdate);
+router.post('/add', protect, cartController.addToCartOrUpdate);
 router.delete('/remove/:productId', protect,cartController.removeItemFromCart);
 
 module.exports = router;
